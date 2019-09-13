418253cf83b78d1c7332d618b611bc9ddca0d056
vukasing95 <55192802+vukasing95@users.noreply.github.com>
2019-09-13 13:05:10 +0200

    Delete extract_files2.R

--- Kaggle1/extract_files2.R
+++
@@ -1,6 +0,0 @@
-list.files(path = "/Users/maraoosterbaan/GitHub/BDS_2019/Kaggle1/youtube-personality/transcripts")
-
-transcript_files = dir("/Users/maraoosterbaan/GitHub/BDS_2019/Kaggle1/youtube-personality/transcripts", full.names = TRUE) 
-head(transcript_files)
-
-
